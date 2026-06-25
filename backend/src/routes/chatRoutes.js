b89f53d6a6968a45b1c6594081ab4dcdd32fc06c
anand raj <git add README.md>
2026-06-25 16:50:11 +0530

    rag bug 2

--- backend/src/routes/chatRoutes.js
+++ backend/src/routes/chatRoutes.js
@@ -4,7 +4,7 @@ import verifyme from "../middleware/auth.middleware.js";
 import { sendmessage ,getchat ,getmessage,deletemessage,upload} from "../controllers/chatController.js";
 import multer from "multer";
 chatrouter.post("/upload",multer().single("image"),verifyme, upload)
-chatrouter.post("/message",verifyme,sendmessage)
+chatrouter.post("/message",multer().single("image"),verifyme,sendmessage)
 chatrouter.get("/",verifyme,getchat)
 chatrouter.get("/:chat",verifyme,getmessage)
 chatrouter.delete("/delete/:chat",verifyme,deletemessage)
